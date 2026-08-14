7b08d0977138ece5ad418d79eb006548c8c9fae0
ali aborady <aboradyali48@gmail.com>
2026-08-14 10:35:44 +0300

    Add project files.

---
+++ GymWep.Presentation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GymWep.Presentation.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
